39626cec14a87e1896798969f45745a8863efc26
Pete <thannakorn.lectival@mail.kmutt.ac.th>
2021-04-05 20:53:18 +0700

    [EDIT] comment some unused code and import firebase_admin

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import os
 from project import app
+
+
 
 if __name__ == '__main__':
     app.run(port = 200)
+    """
+    port = int(os.getenv('PORT',5000))
+    print("Starting app in port %d" % port)
+    app.run(debug=False,port=port,host='0.0.0.0', threaded=True)
+    """
     
--- project/__init__.py
+++ project/__init__.py
@@ -4,11 +4,20 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+from random import randint
+import firebase_admin
+from firebase_admin import credentials
+cred = credentials.Certificate("project/noomfuuproject-f3b69-firebase-adminsdk-mpbf0-f282f03bd0.json")
+firebase_admin.initialize_app(cred)
+
+
 # Flask
 app = Flask(__name__)
-@app.route('/',methods=['POST']) # Using POST as a method
+#@app.route('/', methods=['POST']) can't use POST right now
+@app.route('/')
 
 def MainFunction():
+    """
     #Getting intent from Dailogflow
     question_from_dailogflow_raw = request.get_json(silent=True, force=True)
 
@@ -20,4 +29,30 @@ def MainFunction():
     r.headers['Content-Type'] = 'application/json' #Setting Content Type
 
     return r
+    """
+    return "Noomfuu Hello"
+
+"""
+def generating_answer(question_from_dailogflow_dict):
+    
+    #Print intent that recived from dialogflow.
+    print(json.dumps(question_from_dailogflow_dict, indent=4 ,ensure_ascii=False))
+
+    #Getting intent name form intent that recived from dialogflow.
+    intent_group_question_str = question_from_dailogflow_dict["queryResult"]["intent"]["displayName"] 
+
+    #Select function for answering question
+    if intent_group_question_str == :
+        answer_str = 
+    else: answer_str = "นุ่มฟูไม่เข้าใจ"
+
+    #Build answer dict 
+    answer_from_bot = {"fulfillmentText": answer_str}
+    
+    #Convert dict to JSON
+    answer_from_bot = json.dumps(answer_from_bot, indent=4) 
+    
+    return answer_from_bot
+"""
+
 
